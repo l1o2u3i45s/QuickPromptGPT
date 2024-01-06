37c1d4a6123fa2d05823177efb4e73b0401fb440
xilian <xilian@pixl.world>
2024-01-06 22:28:19 +0800

    add DI

--- QuickPromptGPT/App.xaml.cs
+++ QuickPromptGPT/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI_API;
 
 namespace QuickPromptGPT
 {
@@ -13,5 +15,33 @@ namespace QuickPromptGPT
     /// </summary>
     public partial class App : Application
     {
+
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+
+            services.AddSingleton<MainViewModel>();
+
+            services.AddSingleton<OpenAIAPI>();
+            //services.AddSingleton<IGPTService, GPTService>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            var mainViewModel = _serviceProvider.GetService<MainViewModel>();
+            mainWindow.DataContext = mainViewModel;
+            mainWindow.Show();
+        }
     }
+
 }
