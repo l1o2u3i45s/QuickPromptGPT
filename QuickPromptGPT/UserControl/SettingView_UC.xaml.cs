5541700be8d1c76d946264907e04b30b2e1ec8d6
xilian <xilian@pixl.world>
2024-03-04 00:20:22 +0800

    update settingview UI

---
+++ QuickPromptGPT/UserControl/SettingView_UC.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuickPromptGPT.UserControl
+{
+    /// <summary>
+    /// Interaction logic for SettingView_UC.xaml
+    /// </summary>
+    public partial class SettingView_UC : System.Windows.Controls.UserControl
+    {
+        public SettingView_UC()
+        {
+            InitializeComponent();
+        }
+    }
+}
