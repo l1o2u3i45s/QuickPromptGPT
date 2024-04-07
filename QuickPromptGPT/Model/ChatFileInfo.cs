835fb7e8de0749c1f74b32900ee06b971e2325d1
xilian <xilian@pixl.world>
2024-04-07 23:01:44 +0800

    add file

---
+++ QuickPromptGPT/Model/ChatFileInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace QuickPromptGPT.Model
+{
+    public class ChatFileInfo : ObservableObject
+    {
+        private string _filePath;
+
+        public string FilePath
+        {
+            get => _filePath;
+            set => SetProperty(ref _filePath, value);
+        }
+
+        private bool _isSelectedFile;
+
+        public bool IsSelectedFile
+        {
+            get => _isSelectedFile;
+            set => SetProperty(ref _isSelectedFile, value);
+        }
+    }
+}
